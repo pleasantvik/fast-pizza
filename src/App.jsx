d9d992453878637eb6a1faa87be8fdfa0603d177
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-06-21 03:55:03 +0100

    page route

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,33 @@
+import Home from "./reusables/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order",
+    element: <Order />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+]);
 export const App = () => {
-  return <h1>Fast Pizza</h1>;
+  return <RouterProvider router={router} />;
 };

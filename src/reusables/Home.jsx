a8fed87a02dee889b2851961f9917e0161ec885c
pleasantvik <adedayo.ayoola5683@gmail.com>
2023-06-25 23:56:18 +0100

    tailwind installation

--- src/reusables/Home.jsx
+++ src/reusables/Home.jsx
@@ -1,10 +1,10 @@
 function Home() {
   return (
-    <div>
-      <h1>
+    <div className="">
+      <h1 className="text-center text-xl font-semibold text-yellow-500">
         The best pizza.
         <br />
-        Straight out of the oven, straight to you.
+        Straight out of the oven, straight to you. and me
       </h1>
     </div>
   );
